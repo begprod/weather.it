50766be1dd914eda2be87c577d27195b1414f168
Andrey Aratov <beg431@gmail.com>
2023-01-08 14:49:55 +0700

    added search input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import { FC } from 'react';
 
-function App() {
+export const App:FC = () => {
   return (
-    <div>
-      Hello weather.it
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-1/3">
+        <div className="text-center">
+          emojies image
+        </div>
+        <form>
+          <label htmlFor="default-search"
+                 className="mb-2 text-sm sr-only dark:text-white">Search</label>
+          <div className="relative">
+            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+              <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="none"
+                   stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+              </svg>
+            </div>
+            <input
+              id="default-search"
+              type="search"
+              className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:outline-none"
+              placeholder="Search city"
+              required
+            />
+            <button
+              type="submit"
+              className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2">Search
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
-
-export default App;
