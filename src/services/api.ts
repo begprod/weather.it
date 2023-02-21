b70c6cac68ea1b7f1a9705eb1d45b29cc146ec49
Andrey Aratov <beg431@gmail.com>
2023-02-21 15:18:42 +0700

    load placehodlder image if unsplash reached the limit
    refactored api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import { ISearchItem, ICityWeather, IGetCityImageResponse, ISearchItemList } from '../types';
+import { ISearchItem, ISearchItemList, ICityWeather, IGetCityImageResponse } from '../types';
+import { generateRandomNumber } from '../helpers';
 
 const GEO_DB_API_OPTIONS = {
   method: 'GET',
@@ -9,54 +10,57 @@ const GEO_DB_API_OPTIONS = {
 };
 
 export async function getSearchCitiesList(name: string): Promise<ISearchItemList> {
-  return await fetch(
+  const response =  await fetch(
     `${process.env.REACT_APP_GEO_DB_API_URL}/cities?limit=10&languageCode=en&minPopulation=500000&namePrefix=${name}`,
     GEO_DB_API_OPTIONS
   )
-    .then(async (response) => {
-      const citiesData = await response.json();
+  const citiesData = await response.json();
 
+  return {
+    cities: citiesData.data.map((city: ISearchItem) => {
       return {
-        cities: citiesData.data.map((city: ISearchItem) => {
-          return {
-            id: city.id,
-            name: city.name,
-            country: city.country
-          }
-        })
+        id: city.id,
+        name: city.name,
+        country: city.country
       }
-    });
+    })
+  }
 }
 
 export async function getCityWeather(cityName: string, id: number): Promise<ICityWeather> {
-  return await fetch(`${process.env.REACT_APP_WEATHER_API_URL}/weather?q=${cityName}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
-    .then(async (response) => {
-      if (response.status === 404) {
-        throw new Error('City not found');
-      }
+  const response = await fetch(`${process.env.REACT_APP_WEATHER_API_URL}/weather?q=${cityName}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`)
+  const weatherData = await response.json();
 
-      const weatherData = await response.json();
+  if (response.status === 404) {
+    throw new Error('City not found');
+  }
 
-      return {
-        id,
-        name: weatherData.name,
-        weather: {
-          current: weatherData.main.temp.toFixed(0),
-          feels_like: weatherData.main.feels_like.toFixed(0),
-          main: weatherData.weather[0].main,
-          description: weatherData.weather[0].description,
-        }
-      };
-    });
+  return {
+    id,
+    name: weatherData.name,
+    weather: {
+      current: weatherData.main.temp.toFixed(0),
+      feels_like: weatherData.main.feels_like.toFixed(0),
+      main: weatherData.weather[0].main,
+      description: weatherData.weather[0].description,
+    }
+  };
 }
 
 export async function getCityImage(query: string): Promise<IGetCityImageResponse> {
-  return await fetch(`${process.env.REACT_APP_UNSPLASH_API_URL}/?query=${query}&client_id=${process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY}`)
-    .then(async (response) => {
-      const photosData = await response.json();
+  const response = await fetch(`${process.env.REACT_APP_UNSPLASH_API_URL}/?query=${query}&client_id=${process.env.REACT_APP_UNSPLASH_API_ACCESS_KEY}`)
 
-      return {
-        urls: photosData.urls
+  if (response.status === 403 || response.status === 401 || response.status === 404) {
+    return {
+      urls: {
+        regular: `https://loremflickr.com/500/500?random=${generateRandomNumber(0, 100)}`
       }
-    });
+    }
+  }
+
+  const photosData = await response.json();
+
+  return {
+    urls: photosData.urls
+  };
 }
