7ee5eb1ac3295ac1ffc038faa65a690cf61253f2
Andrey Aratov <beg431@gmail.com>
2023-03-07 14:42:14 +0700

    refactored createDate to Intl

--- src/helpers/createDate.ts
+++ src/helpers/createDate.ts
@@ -1,10 +1,12 @@
 export function createDate() {
   const date = new Date();
-  const minutes = date.getMinutes() > 9 ? date.getMinutes() : `0${date.getMinutes()}`;
-  const hours = date.getHours() > 9 ? date.getHours() : `0${date.getHours()}`;
-  const day = date.getDate() > 9 ? date.getDate() : `0${date.getDate()}`;
-  const month = date.getMonth() + 1 > 9 ? date.getMonth() + 1 : `0${date.getMonth() + 1}`;
-  const year = date.getFullYear();
+  const options = {
+    year: '2-digit',
+    month: 'numeric',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  } as const;
 
-  return `${day}.${month}.${year} ${hours}:${minutes}`;
+  return new Intl.DateTimeFormat('ru', options).format(date);
 }
