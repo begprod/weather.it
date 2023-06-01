40811fd7a00f71df540462d6d157e2c2a568622b
Andrey Aratov <beg431@gmail.com>
2023-06-01 10:10:05 +0700

    remove files

--- src/components/layouts/DefaultLayout.vue
+++ src/components/layouts/DefaultLayout.vue
@@ -1,10 +1,10 @@
-<script setup lang="js">
-import DefaultFooter from "@/components/layouts/partials/DefaultFooter.vue";
-</script>
-
 <template>
   <div class="font-body">
     <slot />
     <DefaultFooter />
   </div>
 </template>
+
+<script setup lang="ts">
+import DefaultFooter from "@/components/layouts/partials/DefaultFooter.vue";
+</script>
