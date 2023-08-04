75b906c093a91d695cf430783662fa51283314b9
Andrey Aratov <beg431@gmail.com>
2023-08-04 21:38:10 +0700

    added test
    BaseSearchSuggestionItem.vue

---
+++ src/components/__tests__/ui/BaseSearchSuggestionItem.spec.ts
@@ -0,0 +1,32 @@
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import { FaMapPin, LaGlobeAsiaSolid } from 'oh-vue-icons/icons';
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import BaseSearchSuggestionItem from '@/components/ui/BaseSearchSuggestionItem.vue';
+
+addIcons(FaMapPin, LaGlobeAsiaSolid);
+
+describe('BaseSearchSuggestionItem', () => {
+  const wrapper = mount(BaseSearchSuggestionItem, {
+    global: {
+      components: {
+        'v-icon': OhVueIcon,
+      },
+    },
+  });
+
+  it('correct render', async () => {
+    expect(wrapper.html()).toContain('svg');
+    expect(wrapper.html()).toContain('ov-icon');
+  });
+
+  it('correct render data from props', async () => {
+    await wrapper.setProps({
+      name: 'Tokyo',
+      country: 'Japan',
+    });
+
+    expect(wrapper.html()).toContain('Tokyo');
+    expect(wrapper.html()).toContain('Japan');
+  });
+});
