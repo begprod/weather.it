a61ee81d13e8143a66bd4ac27d2cd71734bcc97a
Andrey Aratov <beg431@gmail.com>
2023-08-09 21:12:45 +0700

    updated skeleton component
    updated test

--- src/components/__tests__/ui/BaseCardCitySkeleton.spec.ts
+++ src/components/__tests__/ui/BaseCardCitySkeleton.spec.ts
@@ -10,7 +10,7 @@ describe('BaseCardCitySkeleton', () => {
       'relative flex flex-col w-full h-full p-10 bg-white rounded-[20px] shadow-md overflow-hidden isolate',
     );
     expect(wrapper.html()).toContain('relative z-30');
-    expect(wrapper.html()).toContain('animate-pulse flex flex-col w-full mb-4');
+    expect(wrapper.html()).toContain('w-[75px] h-[75px] mt-2 bg-gray-300 rounded-[20px]');
     expect(wrapper.html()).toContain(
       'absolute top-0 left-0 right-0 z-20 w-full h-full bg-gray-600 opacity-50',
     );
