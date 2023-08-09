cfe0243845657a83548ec6474d9565b082336716
Andrey Aratov <beg431@gmail.com>
2023-08-09 21:03:34 +0700

    updated skeleton component

--- src/components/ui/BaseCardCitySkeleton.vue
+++ src/components/ui/BaseCardCitySkeleton.vue
@@ -3,16 +3,20 @@
     class="relative flex flex-col w-full h-full p-10 bg-white rounded-[20px] shadow-md overflow-hidden isolate"
   >
     <div class="relative z-30">
-      <div class="animate-pulse flex flex-col w-full mb-4">
-        <div class="w-5/6 h-4 bg-gray-300 rounded" />
-        <div class="w-1/6 h-4 bg-gray-300 rounded mt-2" />
-        <div class="w-1/6 h-20 mt-5 bg-gray-300 rounded" />
+      <div class="flex flex-col w-full mb-4 animate-pulse">
+        <div class="w-5/12 h-5 bg-gray-300 rounded" />
+        <div class="w-3/12 h-2 bg-gray-300 rounded mt-2" />
       </div>
-      <div class="animate-pulse flex flex-col w-full mb-4">
-        <div class="w-1/3 h-4 bg-gray-300 rounded" />
-        <div class="w-1/2 h-4 mt-2 bg-gray-300 rounded" />
+      <div class="flex flex-row w-full mb-6 animate-pulse">
+        <div class="w-3/12 h-20 mr-2 bg-gray-300 rounded" />
+        <div class="w-1/12 h-10 bg-gray-300 rounded" />
       </div>
-      <div class="animate-pulse flex flex-col w-full mb-4">
+      <div class="flex flex-col w-full mb-4 animate-pulse">
+        <div class="w-6/12 h-4 bg-gray-300 rounded" />
+        <div class="w-3/12 h-4 mt-3 bg-gray-300 rounded" />
+        <div class="w-4/12 h-4 mt-3 bg-gray-300 rounded" />
+      </div>
+      <div class="flex flex-col w-full mb-4 animate-pulse">
         <div class="w-[75px] h-[75px] mt-2 bg-gray-300 rounded-[20px]" />
       </div>
     </div>
