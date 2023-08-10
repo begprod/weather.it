3ec88d5acd80ffd256a34bb1eb668d7f19ccdf4f
Andrey Aratov <beg431@gmail.com>
2023-08-10 18:35:58 +0700

    added test
    BaseDropdownMenu.vue

---
+++ src/components/__tests__/ui/BaseDropdownMenu.spec.ts
@@ -0,0 +1,45 @@
+import { OhVueIcon, addIcons } from 'oh-vue-icons';
+import { HiDotsVertical } from 'oh-vue-icons/icons';
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import { clickOutside } from '@/directives/clickOutsideDirective';
+import BaseDropdownMenu from '@/components/ui/BaseDropdownMenu.vue';
+
+addIcons(HiDotsVertical);
+
+describe('BaseDropdownMenu', () => {
+  const wrapper = mount(BaseDropdownMenu, {
+    props: {
+      isMenuOpen: false,
+    },
+    slots: {
+      default: '<div>Some content</div>',
+    },
+    directives: {
+      'click-outside': clickOutside,
+    },
+    components: {
+      'v-icon': OhVueIcon,
+    },
+  });
+
+  it('correct render slot when menu is open', async () => {
+    await wrapper.setProps({ isMenuOpen: true });
+
+    expect(wrapper.html()).toContain('Some content');
+  });
+
+  it('correct emit toggle event on click', async () => {
+    await wrapper.find('button').trigger('click');
+
+    expect(wrapper.emitted()).toHaveProperty('toggleMenu');
+  });
+
+  it('correct emit close event on directive click outside', async () => {
+    await wrapper.setProps({ isMenuOpen: true });
+
+    document.body.click();
+
+    expect(wrapper.emitted()).toHaveProperty('closeMenu');
+  });
+});
