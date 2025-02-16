13f8dd3fcafe9e9e82c1ebe4d1afa4009cfe61a8
Andrey Aratov <beg431@gmail.com>
2025-02-16 09:52:07 +0300

    refactor: BaseCardCity component css

--- src/components/ui/BaseCardCity/BaseCardCity.vue
+++ src/components/ui/BaseCardCity/BaseCardCity.vue
@@ -1,76 +1,74 @@
 <template>
   <div
     :title="`${city.country}, ${city.name}, ${city.weather.current}°C, Feels like: ${city.weather.feels_like}°C, ${city.weather.description}`"
-    class="city-card group relative p-5 text-white overflow-hidden rounded-[20px] shadow-md isolate md:p-7 lg:p-10 focus:border-none focus:outline-2 focus:outline-orange-500"
+    class="card-city"
     tabindex="0"
   >
-    <div class="relative z-30 select-none">
-      <div class="flex items-baseline">
-        <MapPin class="w-4 h-4 shrink-0 mr-1 lg:w-6 lg:h-6" />
+    <div class="card-city__content">
+      <div class="card-city__city">
+        <MapPin class="icon icon_lg" />
 
-        <div class="flex flex-col max-w-[80%]">
-          <span class="text-xl lg:text-2xl max-w-full truncate" data-test-id="city-name">
-            {{ city.name }}
-          </span>
-          <span class="text-xs" data-test-id="city-country">{{ city.country }}</span>
+        <div class="card-city__city-info">
+          <span class="card-city__name" data-test-id="city-name">{{ city.name }}</span>
+          <span data-test-id="city-country">{{ city.country }}</span>
         </div>
       </div>
-      <div
-        class="flex items-start mt-3 text-5xl font-bold md:mt-5 md:mb-2 md:text-7xl"
-        data-test-id="weather-current"
-      >
-        {{ city.weather.current }}
-        <sub class="-top-1 text-lg font-normal">&#8451;</sub>
-      </div>
-      <div class="flex text-base md:text-lg">
-        feels like:
-        <div class="flex items-start ml-1" data-test-id="weather-feels-like">
-          {{ city.weather.feels_like }}
-          <sub class="top-2 text-[10px] font-normal">&#8451;</sub>
+      <div class="card-city__info">
+        <div class="card-city__degrees card-city__degrees_lg">
+          <span>{{ city.weather.current }}</span>
+          <span>&#8451;</span>
         </div>
       </div>
-      <div class="flex text-base md:text-lg" data-test-id="city-air">
-        air:
-        <div class="flex items-center ml-1">
-          <div class="rounded-full w-4 h-4 shrink-0 mr-1" :class="airQuality"></div>
+      <div class="card-city__info">
+        <span>feels like:</span>
+        <div class="card-city__degrees">
+          <span>{{ city.weather.feels_like }}</span>
+          <span>&#8451;</span>
         </div>
       </div>
-      <div class="text-base md:text-lg md:mb-2" data-test-id="weather-description">
+      <div class="card-city__info">
+        <span>air: </span>
+        <div
+          class="air-quality icon icon_base icon_rounded"
+          :class="airQuality"
+          data-test-id="city-air"
+        ></div>
+      </div>
+      <div class="card-city__info" data-test-id="weather-description">
         {{ city.weather.description }}
       </div>
 
-      <BaseWeatherIcon class="mt-3" :type="city.weather.main.toLowerCase()" />
+      <BaseWeatherIcon class="card-city__weather-icon" :type="city.weather.main.toLowerCase()" />
     </div>
 
     <BaseDropdownMenu
-      class="!absolute top-5 right-5 lg:top-10 lg:right-8 z-30"
+      class="card-city__menu"
       :isMenuOpen="isMenuOpen"
       @toggleMenu="toggleMenu"
       @closeMenu="closeMenu"
     >
       <BaseButton
-        class="w-full flex items-center justify-center p-2 text-red-600 hover:bg-slate-200 transition-all duration-300 ease-in-out"
         view="transparent"
+        text="alert"
         :title="`Remove ${city.name} city`"
-        data-test-id="delete-city-button"
         @click="deleteCity(city.id)"
+        data-test-id="remove-city-button"
       >
-        Remove city
-        <SquareX class="w-5 h-5 ml-1" />
+        <span>Remove city</span>
+        <SquareX class="icon icon_md" />
       </BaseButton>
     </BaseDropdownMenu>
-    <div class="absolute top-0 left-0 right-0 z-20 w-full h-full bg-gray-700 opacity-50" />
+
+    <div class="card-city__paranja" />
+
     <div
-      class="absolute top-0 left-0 right-0 z-10 w-full h-full bg-cover bg-center"
+      class="card-city__image"
       :style="{ backgroundImage: `url(${image})` }"
       data-test-id="city-image"
     />
 
     <Transition name="fade">
-      <div
-        v-if="isLoading"
-        class="absolute top-0 left-0 right-0 z-30 w-full h-full bg-cover bg-center"
-      >
+      <div v-if="isLoading" class="card-city__skeleton z-30">
         <BaseCardCitySkeleton />
       </div>
     </Transition>
@@ -111,17 +109,17 @@ const closeMenu = () => {
 const airQuality = computed(() => {
   switch (props.city.weather.air_quality) {
     case 1:
-      return 'bg-green-500';
+      return 'air-quality_type_1';
     case 2:
-      return 'bg-green-300';
+      return 'air-quality_type_2';
     case 3:
-      return 'bg-yellow-300';
+      return 'air-quality_type_3';
     case 4:
-      return 'bg-orange-400';
+      return 'air-quality_type_4';
     case 5:
-      return 'bg-red-500';
+      return 'air-quality_type_5';
     default:
-      return 'bg-gray-400';
+      return 'air-quality_type_default';
   }
 });
 
@@ -129,3 +127,143 @@ defineExpose({
   toggleMenu,
 });
 </script>
+
+<style scoped>
+.card-city {
+  position: relative;
+  padding: clamp(1.25rem, 3.13vw, 2.5rem);
+  color: var(--white);
+  border-radius: 20px;
+  box-shadow: 0 5px 15px 0 var(--gray);
+  overflow: hidden;
+
+  &:focus {
+    outline: 1px solid var(--gray);
+  }
+}
+
+.card-city__content {
+  position: relative;
+  user-select: none;
+  z-index: 30;
+}
+
+.card-city__city {
+  display: flex;
+  gap: 0.2rem;
+  margin-bottom: 1rem;
+  font-size: var(--typo-size-xs);
+}
+
+.card-city__city-info {
+  display: flex;
+  flex-direction: column;
+  line-height: 1.2;
+  gap: 0.2rem;
+}
+
+.card-city__name {
+  font-size: clamp(var(--typo-size-lg), 2.34vw, var(--typo-size-2xl));
+}
+
+.card-city__info {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  gap: 0.2rem;
+  line-height: 1;
+  font-size: clamp(var(--typo-size-base), 1.76vw, var(--typo-size-lg));
+}
+
+.card-city__degrees {
+  display: flex;
+  gap: 0.2rem;
+  font-size: var(--typo-size-xs);
+
+  span {
+    &:first-child {
+      font-size: var(--typo-size-lg);
+    }
+  }
+}
+
+.card-city__degrees_lg {
+  font-size: var(--typo-size-lg);
+
+  span {
+    &:first-child {
+      font-size: clamp(var(--typo-size-5xl), 7.03vw, var(--typo-size-7xl));
+    }
+  }
+
+  span:nth-child {
+    font-size: inherit;
+  }
+}
+
+.card-city__weather-icon {
+  margin-top: 1.5rem;
+}
+
+.card-city__menu {
+  position: absolute;
+  top: clamp(1.25rem, 3.13vw, 2.5rem);
+  right: clamp(1.25rem, 3.13vw, 2.5rem);
+  z-index: 30;
+}
+
+.card-city__image {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background-size: cover;
+  background-position: center;
+  z-index: 10;
+}
+
+.card-city__skeleton {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  z-index: 30;
+}
+
+.card-city__paranja {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  background: var(--slate-700);
+  opacity: 0.5;
+  z-index: 20;
+}
+
+.air-quality_type_default {
+  background-color: var(--gray);
+}
+
+.air-quality_type_1 {
+  background-color: var(--green-500);
+}
+
+.air-quality_type_2 {
+  background-color: var(--green-300);
+}
+
+.air-quality_type_3 {
+  background-color: var(--yellow-300);
+}
+
+.air-quality_type_4 {
+  background-color: var(--orange-400);
+}
+
+.air-quality_type_5 {
+  background-color: var(--red-500);
+}
+</style>
