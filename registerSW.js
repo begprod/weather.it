e4d301fa7b872dd776e7e6b1ea029052c228af5a
Andrey Aratov <beg431@gmail.com>
2025-04-23 11:27:32 +0300

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/weather.it/sw.js', { scope: '/weather.it/' })})}
